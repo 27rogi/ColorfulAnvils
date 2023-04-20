520b538392910402024a547103d7173a874dfec1
rogi27 <27rogi@pm.me>
2023-04-21 01:22:54 +0300

    fix: api changed to match 1.18.2

--- src/main/java/space/ryzhenkov/colorfulanvils/mixins/AnvilScreenHandlerMixin.java
+++ src/main/java/space/ryzhenkov/colorfulanvils/mixins/AnvilScreenHandlerMixin.java
@@ -1,9 +1,10 @@
 package space.ryzhenkov.colorfulanvils.mixins;
 
-import eu.pb4.placeholders.api.TextParserUtils;
+import eu.pb4.placeholders.TextParser;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.*;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,13 +25,13 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
     @ModifyArg(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCustomName(Lnet/minecraft/text/Text;)Lnet/minecraft/item/ItemStack;", ordinal = 0))
     public Text updateResult(Text name) {
         // disable formatting for players with 'colorfulanvils.use' permission set to false, otherwise allow formatting
-        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return Text.literal(this.newItemName);
-        return TextParserUtils.formatTextSafe(this.newItemName);
+        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return new LiteralText(this.newItemName);
+        return TextParser.parseSafe(this.newItemName);
     }
 
     @ModifyArg(method = "setNewItemName", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCustomName(Lnet/minecraft/text/Text;)Lnet/minecraft/item/ItemStack;", ordinal = 0))
     public Text newItemName(Text name) {
-        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return Text.literal(this.newItemName);
-        return TextParserUtils.formatTextSafe(this.newItemName);
+        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return new LiteralText(this.newItemName);
+        return TextParser.parseSafe(this.newItemName);
     }
 }
