103d1db8ec2f53b5367b2099b54a953da9335f34
JustAlittleWolf <jakobi5000@gmail.com>
2024-01-16 13:39:58 +0100

    feat: non-italic item names
    Changes item names to be non-italic when applying a custom formatting.
    Examples:
    Item name: "Name" will still be italic
    Item name: "<red>Name" will be red and not italic
    Item name: "<i><red>Name" will be red and italic
    Item name: "<reset>Name" will still be italic, this is an issue with the detection method.
    Item name: "<white>Name" will not be italic

--- src/main/java/space/ryzhenkov/colorfulanvils/mixins/AnvilScreenHandlerMixin.java
+++ src/main/java/space/ryzhenkov/colorfulanvils/mixins/AnvilScreenHandlerMixin.java
@@ -4,10 +4,14 @@ import eu.pb4.placeholders.api.TextParserUtils;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.*;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
@@ -23,14 +27,29 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
     @ModifyArg(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCustomName(Lnet/minecraft/text/Text;)Lnet/minecraft/item/ItemStack;", ordinal = 0))
     public Text updateResult(Text name) {
-        // disable formatting for players with 'colorfulanvils.use' permission set to false, otherwise allow formatting
-        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return Text.literal(this.newItemName);
-        return TextParserUtils.formatTextSafe(this.newItemName);
+        return customTextFormatting(name);
     }
 
     @ModifyArg(method = "setNewItemName", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCustomName(Lnet/minecraft/text/Text;)Lnet/minecraft/item/ItemStack;", ordinal = 0))
     public Text newItemName(Text name) {
-        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return Text.literal(this.newItemName);
-        return TextParserUtils.formatTextSafe(this.newItemName);
+        return customTextFormatting(name);
+    }
+
+    @Unique
+    private Text customTextFormatting(Text defaultText) {
+        // disable formatting for players with 'colorfulanvils.use' permission set to false, otherwise allow formatting
+        if (!Permissions.check(this.player, "colorfulanvils.use", true)) return defaultText;
+
+        Text formattedText = TextParserUtils.formatTextSafe(this.newItemName);
+        // check if the new item name has a custom format
+        boolean hasFormat = switch (formattedText.getSiblings().size()) {
+            case 0 -> false;
+            // this detection fails if the item name is "<reset>Itemname"
+            case 1 -> !formattedText.getSiblings().get(0).getStyle().equals(Style.EMPTY);
+            default -> true;
+        };
+        // if the new item name has a custom format, disable italics
+        if (hasFormat) return formattedText.copy().setStyle(formattedText.getStyle().withItalic(false));
+        return formattedText;
     }
 }
